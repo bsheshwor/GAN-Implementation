2b8f7e252fa2a42c55c8799ea12f3225d629470c
Bisheshwor Neupane <bn02433818@student.ku.edu.np>
2023-01-23 11:03:58 +0545

    DCGAN

---
+++ DCGAN-paper-implementation/model.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+
+def get_noise(n_samples, z_dim, device='cpu'):
+    return torch.randn(n_samples, z_dim, device=device)
+
+class Generator(nn.Module):
+    def __init__(self, z_dim=10, im_channel = 1, hidden_dim = 64):
+        super(Generator,self).__init__()
+        self.z_dim = z_dim
+        self.gen = nn.Sequential(
+            self._generator_block(z_dim, hidden_dim*4),
+            self._generator_block(hidden_dim*4, hidden_dim*2, kernel_size = 4, stride=1),
+            self._generator_block(hidden_dim*2, hidden_dim),
+            self._generator_block(hidden_dim, im_channel, kernel_size = 4, final_layer=True)
+        )
+    
+    def _generator_block(self, input_channels, output_channels, kernel_size = 3, stride = 2, final_layer=False):
+        if not final_layer:
+            return nn.Sequential(
+                nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride),
+                nn.BatchNorm2d(output_channels),
+                nn.ReLU(inplace = True)
+            )
+        else:
+            return nn.Sequential(
+                nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride),
+                nn.Tanh()
+            )
+    
+    def unsqueeze_noise(self, noise):
+        return noise.view(len(noise),self.z_dim, 1,1)
+    
+    def forward(self, noise):
+        X = self.unsqueeze_noise(noise)
+        return self.gen(X)
+
+class Discriminator(nn.Module):
+    def __init__(self, im_channel=1, hidden_dim=16):
+        super(Discriminator, self).__init__()
+        self.disc = nn.Sequential(
+            self._discriminator_block(im_channel, hidden_dim),
+            self._discriminator_block(hidden_dim, hidden_dim*2),
+            self._discriminator_block(hidden_dim*2, 1, final_layer=True)
+        )
+
+    def _discriminator_block(self, input_channels, output_channels, kernel_size=4, stride=2, final_layer=False):
+        if not final_layer:
+            return nn.Sequential(
+                nn.Conv2d(input_channels, output_channels, kernel_size, stride),
+                nn.BatchNorm2d(output_channels),
+                nn.LeakyReLU(0.2, inplace=True)
+            )
+        else:
+            return nn.Sequential(
+                nn.Conv2d(input_channels, output_channels, kernel_size, stride)
+            )
+
+    def forward(self, image):
+        disc_pred = self.disc(image)
+        return disc_pred.view(len(disc_pred), -1)
+
+def initialize_weights(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        nn.init.normal_(m.weight, 0.0, 0.02)
+    if isinstance(m, nn.BatchNorm2d):
+        nn.init.normal_(m.weight, 0.0, 0.02)
+        nn.init.constant_(m.bias, 0)
+
+
+
---
+++ DCGAN-paper-implementation/train.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from model import Discriminator, Generator, initialize_weights, get_noise
+
+writer_fake = SummaryWriter(f"runs/GAN_MNIST/fake")
+writer_real = SummaryWriter(f"runs/GAN_MNIST/real")
+step = 0
+
+criterion = nn.BCEWithLogitsLoss()
+z_dim = 64
+display_step = 500
+batch_size = 10
+# A learning rate of 0.0002 works well on DCGAN
+lr = 0.0002
+
+# These parameters control the optimizer's momentum, which you can read more about here:
+# https://distill.pub/2017/momentum/ but you don’t need to worry about it for this course!
+beta_1 = 0.5 
+beta_2 = 0.999
+device = 'cuda'
+
+# You can tranform the image values to be between -1 and 1 (the range of the tanh activation)
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,)),
+])
+
+dataset = datasets.MNIST(root="dataset/", transform = transform, download = True)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+gen = Generator(z_dim).to(device)
+gen_opt = torch.optim.Adam(gen.parameters(), lr=lr, betas=(beta_1, beta_2))
+disc = Discriminator().to(device) 
+disc_opt = torch.optim.Adam(disc.parameters(), lr=lr, betas=(beta_1, beta_2))
+
+gen = gen.apply(initialize_weights)
+disc = disc.apply(initialize_weights)
+
+n_epochs = 5
+cur_step = 0
+mean_generator_loss = 0
+mean_discriminator_loss = 0
+
+for epoch in range(n_epochs):
+    # Dataloader returns the batches
+    for batch_id, (real, _) in enumerate(dataloader):
+        cur_batch_size = len(real)
+        real = real.to(device)
+
+        ## Update discriminator ##
+        disc_opt.zero_grad()
+        fake_noise = get_noise(cur_batch_size, z_dim, device=device)
+        fake = gen(fake_noise)
+        disc_fake_pred = disc(fake.detach())
+        disc_fake_loss = criterion(disc_fake_pred, torch.zeros_like(disc_fake_pred))
+        disc_real_pred = disc(real)
+        disc_real_loss = criterion(disc_real_pred, torch.ones_like(disc_real_pred))
+        disc_loss = (disc_fake_loss + disc_real_loss) / 2
+
+        # Keep track of the average discriminator loss
+        mean_discriminator_loss += disc_loss.item() / display_step
+        # Update gradients
+        disc_loss.backward(retain_graph=True)
+        # Update optimizer
+        disc_opt.step()
+
+        ## Update generator ##
+        gen_opt.zero_grad()
+        fake_noise_2 = get_noise(cur_batch_size, z_dim, device=device)
+        fake_2 = gen(fake_noise_2)
+        disc_fake_pred = disc(fake_2)
+        gen_loss = criterion(disc_fake_pred, torch.ones_like(disc_fake_pred))
+        gen_loss.backward()
+        gen_opt.step()
+
+        # Keep track of the average generator loss
+        mean_generator_loss += gen_loss.item() / display_step
+
+        ## Visualization code ##
+        if cur_step % display_step == 0 and cur_step > 0:
+            print(f"Step {cur_step}: Generator loss: {mean_generator_loss}, discriminator loss: {mean_discriminator_loss}")
+            mean_generator_loss = 0
+            mean_discriminator_loss = 0
+        
+        with torch.no_grad():
+            fake = gen(fake_noise).reshape(-1,1,28,28)
+            data = real.reshape(-1,1,28,28)
+            img_grid_fake = torchvision.utils.make_grid(fake,normalize=True)
+            img_grid_real = torchvision.utils.make_grid(data, normalize=True)
+
+            writer_fake.add_image(
+                "Mnist Fake Image", img_grid_fake, global_step=step
+            )
+            writer_fake.add_image(
+                "Mnist fake Images", img_grid_fake, global_step=step
+            )
+            step += 1
+        cur_step += 1
+
